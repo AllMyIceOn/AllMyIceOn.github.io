c90ddffcd308685a024caf4da7777a2f6ef666ae
AllMyIceOn <110044098+AllMyIceOn@users.noreply.github.com>
2022-08-01 14:47:55 +0200

    kancelaria

---
+++ kancelaria/kancelaria.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+	<header>
+	</header>
+	<nav>
+	</nav>
+	<section class="sekcja">
+	</section>
+
+
+</body>
+</html>
